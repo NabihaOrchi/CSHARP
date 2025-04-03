38990dd54b3454c5fe7b6217ac2879827d5c4b2e
Nabiha Tasnim Orchi <nabihaorchi@gmail.com>
2025-04-04 05:13:19 +0600

    error

---
+++ variables.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int tableHeight = 29;
+        string tableName = "table";
+
+        Console.WriteLine(tableHeight);
+    }
+}
