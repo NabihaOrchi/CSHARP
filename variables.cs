ff05ae00211183308fedffd902f0ee5ca7a2faa6
Nabiha Tasnim Orchi <nabihaorchi@gmail.com>
2025-04-04 05:14:01 +0600

    [[

--- variables.cs
+++ variables.cs
@@ -1,10 +1,5 @@
-﻿internal class Program
-{
-    public static void Main(string[] args)
-    {
+﻿
         int tableHeight = 29;
         string tableName = "table";
 
         Console.WriteLine(tableHeight);
-    }
-}
