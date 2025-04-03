70d41c213560ac1d898ece4fd8392fc279a72e94
Nabiha Tasnim Orchi <nabihaorchi@gmail.com>
2025-04-04 05:29:27 +0600

    ok

--- variables.cs
+++ variables.cs
@@ -1,5 +1,24 @@
-﻿
-        int tableHeight = 29;
-        string tableName = "table";
+﻿using System;
 
-        Console.WriteLine(tableHeight);
+namespace add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            
+            Console.Write("Enter first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            
+            int result;
+            result = number1 + number2;
+            
+            Console.WriteLine("Sum of two Numbers: " + result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
