ee706d24aab1033ed4421a0ccd609de5e5bb4dac
Nabiha Tasnim Orchi <nabihaorchi@gmail.com>
2025-04-04 20:33:46 +0600

    sum

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello World! this is Nabiha");
-Console.ReadLine();
+namespace add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            
+            Console.Write("Enter first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            
+            int result;
+            result = number1 + number2;
+            
+            Console.WriteLine("Sum of two Numbers: " + result.ToString());
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
