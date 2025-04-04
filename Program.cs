1767900fa9283200a5d48eb3bf34b606ae137d69
Nabiha Tasnim Orchi <nabihaorchi@gmail.com>
2025-04-04 20:38:06 +0600

    table

--- Program.cs
+++ Program.cs
@@ -1,26 +1,3 @@
-﻿using System;
-
-namespace add
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int number1, number2;
-            
-            Console.Write("Enter first number: ");
-            number1 = Convert.ToInt32(Console.ReadLine());
-            
-            Console.Write("Enter second number: ");
-            number2 = Convert.ToInt32(Console.ReadLine());
-            
-            int result;
-            result = number1 + number2;
-            
-            Console.WriteLine("Sum of two Numbers: " + result.ToString());
-            Console.ReadLine();
-        }
-        
-        
-    }
-}
+﻿int tableHeight = 50;
+int tableWidth = 150;
+Console.WriteLine(tableWidth);
