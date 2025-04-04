087db5643cb699b5c7f5428d9fe711db95fb60bc
Nabiha Tasnim Orchi <nabihaorchi@gmail.com>
2025-04-04 20:41:16 +0600

    chair

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿int tableHeight = 50;
 int tableWidth = 150;
 Console.WriteLine(tableWidth);
+Console.WriteLine(tableHeight);
+Console.ReadLine("please write you chair requirements ");
+Console.ReadLine("ChairWidth ");
+Console.ReadLine("ChairHeight ");
